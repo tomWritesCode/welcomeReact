2374b4ea764b1bd953c0feba71f93a7228bf2d17
Thomas Whitaker <thomaswhitaker@protonmail.com>
2018-03-14 17:35:57 +1100

    1

---
+++ src/components/footerComponent/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+	render() {
+		return (
+			<footer>
+				Footer Text
+			</footer>
+		);
+	}
+}
+
+export default Footer;
---
+++ src/components/pages/products.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Products extends Component {
+	render() {
+		return (
+      <div className="container-fluid">  
+        <h1>
+          Products
+        </h1>
+
+        <p>
+          Lots of information
+        </p>
+      </div>
+    );
+	}
+}
+
+export default Products;
